dafcc5663094ef30e17840f05d0be16e119c0534
Steve Kimoi <moenisteve@gmail.com>
2022-10-04 11:45:28 +0300

    the basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect } from 'react'
+import Quiz from './components/Quiz'
+import Data from './Data'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [questions, setQuestions] = useState(Data.data.questions); 
+  const [answers, setAnswers] = useState([]); 
+
+  function settingAnswers() {
+    // for(let i = 0; i <= questions.length; i++){
+    //   // setAnswers(Data.data.questions[i].answer); 
+    //   answers.push(Data.data.questions[i].answer)
+    // }
+    questions.forEach( (question) => {
+      answers.push(question.answer)
+    })
+    console.log("Answers: ", answers);
+  } 
+
+  useEffect( () => {
+    settingAnswers()
+  }, [questions]); 
 
   return (
     <div className="App">
-         
+         <Quiz questions={questions} answers={answers}/> 
+         <p>
+           Data loading
+         </p>
     </div>
   )
 }
