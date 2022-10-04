3f82d7d85c52cfd868f12003861b9665fc1c5e63
Steve Kimoi <moenisteve@gmail.com>
2022-10-04 11:44:21 +0300

    mock data

---
+++ src/Data.js
@@ -0,0 +1,47 @@
+export default {
+    "success": true, 
+    "data": {
+        "questions": [
+            {
+                "qn": "How would one say goodbye in spanish", 
+                "a": "Adios", 
+                "b": "Hola", 
+                "c": "Au Revoir", 
+                "d": "Salir", 
+                "answer": "Adios", 
+            }, 
+            {
+                "qn": "Which best selling toy of 1983 caused hysteria, resulting to riots breaking in stores", 
+                "a": "Cabbage Patch Kids", 
+                "b": "Transformers", 
+                "c": "Care Bears", 
+                "d": "Rubik's Cube", 
+                "answer": "Transformers", 
+            }, 
+            {
+                "qn": "What is the hottest planet in our solar system", 
+                "a": "Mercury", 
+                "b": "Venus", 
+                "c": "Mars", 
+                "d": "Saturn", 
+                "answer": "Venus", 
+            }, 
+            {
+                "qn": "In which country was the ceaser salad invented", 
+                "a": "Italy", 
+                "b": "Portugal", 
+                "c": "Mexico", 
+                "d": "France", 
+                "answer": "Mexico", 
+            }, 
+            {
+                "qn": "How many Hearts does an Octopus have", 
+                "a": "One", 
+                "b": "Two", 
+                "c": "Three", 
+                "d": "Four", 
+                "answer": "Three", 
+            }
+        ]
+    }
+}
