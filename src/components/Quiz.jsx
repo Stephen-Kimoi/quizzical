51e226054d04526e6a0896f36424ba86e352e634
Steve Kimoi <moenisteve@gmail.com>
2022-10-04 11:44:39 +0300

    components

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+
+const Quiz = ({questions, answers}) => {
+  const [choosenAnswers, setChoosenAnswers] = useState([]); 
+  const [correctAnswerSum, setCorrectAnswerSum] = useState(0); 
+
+  function chooseAnswers(value) {
+    setChoosenAnswers( prevAnswers => {
+      return [
+        ...prevAnswers, 
+        value
+      ]
+    })
+    console.log(choosenAnswers)
+  }
+
+  function submitAnswers() {
+    // for(let answer in choosenAnswers) {
+    //   console.log("Answer in submit answers funct: ",answer)
+    // }
+    let correctAnswerSum = 0; 
+
+    choosenAnswers.forEach( selectedAanswer => {
+      answers.forEach( answer => {
+        if (answer === selectedAanswer){
+          correctAnswerSum++;  
+        }
+      })
+    }) 
+
+    console.log(correctAnswerSum);
+    setCorrectAnswerSum(correctAnswerSum);  
+  } 
+  
+  const questionsDiv = questions.map( question => {
+    return (
+        <div className='question'>
+            <p className='question-title'>{ question.qn }</p> 
+            <ul className='question-answers'> 
+                <li className='question-answer' onClick={ () => chooseAnswers(question.a)}>{ question.a }</li>
+                <li className='question-answer' onClick={ () => chooseAnswers(question.b)}>{ question.b }</li>
+                <li className='question-answer' onClick={ () => chooseAnswers(question.c)}>{ question.c }</li>
+                <li className='question-answer' onClick={ () => chooseAnswers(question.d)}>{ question.d }</li>
+            </ul>
+        </div>
+    )
+  })
+  return (
+    <div className='quiz'> 
+      <div className='quiz-container'> 
+      <p>Quiz section</p>
+        {
+         questionsDiv
+        }
+      <button onClick={submitAnswers}>Submit answers</button>  
+      <p>You've gotten {correctAnswerSum} out of 5</p>
+      </div>
+    </div>
+  )
+}
+
+export default Quiz
