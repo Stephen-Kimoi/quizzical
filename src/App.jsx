81b7c35862e7bdecf8f4d1f99145f73dbf54b49b
Steve Kimoi <moenisteve@gmail.com>
2022-10-06 10:04:57 +0300

    storing choosen answers in the choosenAnswers state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { nanoid } from 'nanoid'
 import Blob5 from './assets/blob5.svg' 
 import Blob1 from './assets/blobs.svg'
 import Quiz from './components/Quiz'
@@ -6,28 +7,126 @@ import Data from './Data'
 import './App.css'
 
 function App() {
-  const [questions, setQuestions] = useState(Data.data.questions); 
+  const [questions, setQuestions] = useState(
+    Data.data.questions.map( question => {
+      return {
+        ...question, 
+        choosen: false, 
+        id: nanoid()
+      }
+    })
+  ); 
   const [answers, setAnswers] = useState([]); 
+  const [questionsDiv, setQuestionsDiv] = useState(); 
+  const [choosenAnswers, setChoosenAnswers] = useState([]); 
+  const [selectedQuestions, setSelectedQuestions] = useState([]);
+  const [correctAnswerSum, setCorrectAnswerSum] = useState(0); 
+
+
+
+  const randomNumber = () => {
+    return Math.floor(Math.random() * 5)
+  }
+
+  console.log("Random number: ", randomNumber())
+
+  const slicedNumber = () => {
+    return (randomNumber() + 5); 
+  }
+
+  console.log("Sliced number: ", slicedNumber())
+
+  function chooseAnswers(answer, questionId) {
+    console.log(answer, questionId); 
+    
+    setChoosenAnswers(prevAnswers => [...prevAnswers, answer])
+    
+    // setQuestions( prevQuestion => prevQuestion.forEach( question => {
+    //   return question.id === questionId ? {
+    //     ...question, 
+    //     choosen: !question.choosen
+    //   } : 
+    //   question
+    // }))
+
+  }
+
+  function settingQuestions() {
+    setQuestionsDiv(); 
+    setAnswers();
+
+    let selectedQuestions = questions.slice(randomNumber(), slicedNumber()); 
+    setAnswers( 
+      selectedQuestions.forEach( (question) => {
+        answers.push(question.answer)
+      })
+    )
+
+    console.log("Selcted answers: ", answers); 
+    
+    setQuestionsDiv(
+      selectedQuestions.map( question => {
+        return (
+           <div className='question' key={question.id}>
+              <p className='question-title'>{ question.qn }</p> 
+              <ul className='question-answers'> 
+                 <li className='question-answer' onClick={ () => chooseAnswers(question.a, question.id)}>{ question.a }</li>
+                 <li className='question-answer' onClick={ () => chooseAnswers(question.b, question.id)}>{ question.b }</li>
+                 <li className='question-answer' onClick={ () => chooseAnswers(question.c, question.id)}>{ question.c }</li>
+                 <li className='question-answer' onClick={ () => chooseAnswers(question.d, question.id)}>{ question.d }</li>
+             </ul>
+         </div>
+        )
+      })
+    )
+
+    console.log("Selected questions: ", selectedQuestions)
+  }
+
+  function submitAnswers() {
+    let correctAnswerSum = 0; 
+
+    // choosenAnswers.forEach( selectedAnswer => {
+    //   answers.forEach( answer => {
+    //     if (answer === selectedAnswer){
+    //       correctAnswerSum++;  
+    //     }
+    //   })
+    // }) 
+
+    // console.log(correctAnswerSum);
+    // setCorrectAnswerSum(correctAnswerSum);  
+
+    console.log(choosenAnswers.length); 
+    console.log(choosenAnswers); 
+  } 
 
   function settingAnswers() {
     // for(let i = 0; i <= questions.length; i++){
     //   // setAnswers(Data.data.questions[i].answer); 
     //   answers.push(Data.data.questions[i].answer)
     // }
-    questions.forEach( (question) => {
-      answers.push(question.answer)
-    })
-    console.log("Answers: ", answers);
   } 
 
-  useEffect( () => {
-    settingAnswers()
-  }, [questions]); 
+
+  // useEffect( () => {
+  //   settingAnswers()
+  // }, [questions]); 
 
   return (
     <div className="App">
       <img className="blob-five" src={Blob5} alt='blob 5' /> 
-      <Quiz questions={questions} answers={answers}/> 
+
+      <Quiz 
+        questions={questions} 
+        answers={answers} 
+        randomNumber={randomNumber} 
+        slicedNumber={slicedNumber} 
+        questionsDiv={questionsDiv}
+        settingQuestions={settingQuestions} 
+        submitAnswers={submitAnswers}
+        />
+
       <img className="blob-one" src={Blob1} alt='blob 1' /> 
     </div>
   )
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,57 +1,17 @@
 import React, { useEffect, useState } from 'react';
 
-const Quiz = ({questions, answers}) => {
-  const [choosenAnswers, setChoosenAnswers] = useState([]); 
-  const [correctAnswerSum, setCorrectAnswerSum] = useState(0); 
+const Quiz = ({questions, answers, randomNumber, slicedNumber, questionsDiv, settingQuestions, submitAnswers}) => {
 
-  function chooseAnswers(value) {
-    setChoosenAnswers( prevAnswers => {
-      return [
-        ...prevAnswers, 
-        value
-      ]
-    })
-    console.log(choosenAnswers)
-  }
-
-  function submitAnswers() {
-    // for(let answer in choosenAnswers) {
-    //   console.log("Answer in submit answers funct: ",answer)
-    // }
-    let correctAnswerSum = 0; 
-
-    choosenAnswers.forEach( selectedAanswer => {
-      answers.forEach( answer => {
-        if (answer === selectedAanswer){
-          correctAnswerSum++;  
-        }
-      })
-    }) 
-
-    console.log(correctAnswerSum);
-    setCorrectAnswerSum(correctAnswerSum);  
-  } 
-  
-  const questionsDiv = questions.map( question => {
-    return (
-        <div className='question'>
-            <p className='question-title'>{ question.qn }</p> 
-            <ul className='question-answers'> 
-                <li className='question-answer' onClick={ () => chooseAnswers(question.a)}>{ question.a }</li>
-                <li className='question-answer' onClick={ () => chooseAnswers(question.b)}>{ question.b }</li>
-                <li className='question-answer' onClick={ () => chooseAnswers(question.c)}>{ question.c }</li>
-                <li className='question-answer' onClick={ () => chooseAnswers(question.d)}>{ question.d }</li>
-            </ul>
-        </div>
-    )
-  })
   return (
     <div className='quiz'> 
       <div className='quiz-container'> 
         {
          questionsDiv
         }
-      <button className='check-answers' onClick={submitAnswers}>Check answers</button>  
+      <div className='buttons'>
+        <button className='check-answers' onClick={submitAnswers}>Check answers</button>  
+        <button className='check-answers' onClick={settingQuestions}>New Questions</button>  
+      </div>
       {/* <p>You've gotten {correctAnswerSum} out of 5</p> */}
       </div>
     </div>
