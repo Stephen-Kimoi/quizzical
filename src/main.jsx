8e1a617e4f9de01ff458f3fd9338ad0a9c73ee58
Steve Kimoi <moenisteve@gmail.com>
2022-10-07 09:32:25 +0300

    adding the routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Start from './components/Start'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+     <Route path={'/'} element={<Start />} /> 
+     <Route path={'/game'} element={<App />} /> 
+    </Routes>
+  </BrowserRouter>
 )
